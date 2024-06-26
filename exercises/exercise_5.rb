7f9a34591a5e5e1e6b327e69186c9323546c7daf
Kim O'Dell <kimbo.odell@gmail.com>
2024-06-26 18:34:38 -0400

    Add querys for annual revenue calculations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+# Output total annual revenue
+puts Store.sum(:annual_revenue)
+# Output average annual revenue
+puts Store.average(:annual_revenue)
+
+# Calculate and output the number of stores generating $1M or more in annual sales
+puts Store.where("annual_revenue >= ?", 1000000).count
