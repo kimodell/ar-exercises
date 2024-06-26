2ebed63c18ce8579f626f7dfd6007f268f5aca48
Kim O'Dell <kimbo.odell@gmail.com>
2024-06-26 19:44:31 -0400

    Add validations to store model

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,14 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  validate :carry_mens_or_womens_apparel
+
+  def carry_mens_or_womens_apparel
+    unless mens_apparel || womens_apparel
+      errors.add(:base, "Store must carry mens or womens apparel")
+    end
+  end
 end
